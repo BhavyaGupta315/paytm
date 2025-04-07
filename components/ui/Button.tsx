e2c25c2068ac1d9e2c95955a2949840401d97d2f
BhavyaGupta315 <bhavyagupta315@gmail.com>
2025-04-07 06:17:40 +0530

    Button fixed

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -9,10 +9,14 @@ export const Button = ({label, onClick} : ButtonProps) => {
     return (
         <button
           onClick={onClick}
-          className="inline-flex items-center justify-center text-white bg-gray-800 hover:bg-gray-900 
-          focus:outline-none  focus:ring-gray-300 font-medium 
-          rounded-lg text-sm px-5 py-2.5 cursor-pointer w-auto dark:bg-white/[0.2] 
-          dark:hover:bg-white/[0.1] dark:border-gray-700"
+          className="inline-flex items-center justify-center 
+         text-white bg-gray-800 hover:bg-gray-900 
+         dark:text-gray-900 dark:bg-gray-300 dark:hover:bg-gray-400 
+         focus:outline-none focus:ring-2 focus:ring-gray-300 
+         dark:focus:ring-gray-500 
+         rounded-lg text-sm px-5 py-2.5 
+         font-semibold text-center
+         cursor-pointer transition duration-150 ease-in-out"
         >
           {label}
         </button>
